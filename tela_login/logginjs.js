dd0ab0400963b8693ee7af076f2b3c5722a6525b
FernandoLucasDev <nandopm2004@gmail.com>
2022-05-26 15:10:33 -0300

    site musica crud

---
+++ tela_login/logginjs.js
@@ -0,0 +1,19 @@
+var time = 1;
+        setInterval(mImg, 5000);
+        function mImg(){
+            var foto = document.getElementById("body");
+            
+            switch(time){
+                case 1: 
+                    foto.src = foto.style.backgroundImage = "url('img1.jpg')";
+                    break;
+                case 2:
+                    foto.src = foto.style.backgroundImage = "url('img2.jpg')";
+                    break;
+                case 3:
+                    foto.src = foto.style.backgroundImage = "url('img3.jpg')";
+                    time = 1;
+                    break;
+            }
+            time++;
+        }
