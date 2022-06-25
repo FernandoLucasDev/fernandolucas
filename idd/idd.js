a0005982f5a43477807f933729bf17e02f9fe97f
FernandoLucasDev <nandopm2004@gmail.com>
2022-06-25 15:29:23 -0300

    .

---
+++ idd/idd.js
@@ -0,0 +1,17 @@
+function criar_nota(){
+
+    var text = document.getElementById("notaconteudo").value;
+    var bgC = document.getElementById("cornota").value;
+    var cT = document.getElementById("cortext").value;
+
+    var pai = document.getElementById("notas");
+    var filho = document.createElement("div");
+    pai.appendChild(filho);
+    var texto = document.createTextNode(text);
+    filho.appendChild(texto);
+    filho.classList.add("card");
+    filho.id  = 'nota';
+    filho.style.backgroundColor=bgC;
+    filho.style.color=cT;
+    
+}
